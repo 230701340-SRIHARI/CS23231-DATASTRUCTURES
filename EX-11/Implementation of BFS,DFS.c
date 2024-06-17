7abe8c4dc64dd183cb60104ea92d3077a6538207
230701340-SRIHARI <230701340@rajalakshmi.edu.in>
2024-06-17 22:04:44 +0530

    Update Implementation of BFS,DFS.c

--- EX-11/Implementation of BFS,DFS.c
+++ EX-11/Implementation of BFS,DFS.c
@@ -1,226 +1,148 @@
 NAME: SRIHARI S
 ROLL.NO: 230701340
-EX-10: Implementation of AVL Tree
+Ex-11: Implementation of BFS and DFS
 
 
 
-#include <stdio.h>
-#include <stdlib.h>
-
-typedef struct Node {
-    int key;
-    struct Node* left;
-    struct Node* right;
-    int height;
-} Node;
-
-int height(Node* node) {
-    if (node == NULL)
-        return 0;
-    return node->height;
-}
-
-int max(int a, int b) {
-    return (a > b) ? a : b;
-}
-
-Node* newNode(int key) {
-    Node* node = (Node*)malloc(sizeof(Node));
-    node->key = key;
-    node->left = NULL;
-    node->right = NULL;
-    node->height = 1; 
-    return node;
-}
-
-Node* rightRotate(Node* y) {
-    Node* x = y->left;
-    Node* T2 = x->right;
-
-    x->right = y;
-    y->left = T2;
-
-    y->height = max(height(y->left), height(y->right)) + 1;
-    x->height = max(height(x->left), height(x->right)) + 1;
 
 
-    return x;
-}
+#include<stdio.h>
+#include<stdlib.h>
+#define size 7
+int s[size];
+int top=-1;
+int pop();
+void push(int);
+int queue[size];
+int front = -1, rear = -1;
+void dfs();
+void bfs();
 
+int isEmpty() { return front == -1 && rear == -1; }
 
-Node* leftRotate(Node* x) {
-    Node* y = x->right;
-    Node* T2 = y->left;
+int isFull() { return rear == size - 1; }
 
-
-    y->left = x;
-    x->right = T2;
-
-    
-    x->height = max(height(x->left), height(x->right)) + 1;
-    y->height = max(height(y->left), height(y->right)) + 1;
-
-   
-    return y;
-}
-
-
-int getBalance(Node* N) {
-    if (N == NULL)
-        return 0;
-    return height(N->left) - height(N->right);
-}
-
-
-Node* insert(Node* node, int key) {
-    
-    if (node == NULL)
-        return newNode(key);
-
-    if (key < node->key)
-        node->left = insert(node->left, key);
-    else if (key > node->key)
-        node->right = insert(node->right, key);
-    else 
-        return node;
-
- 
-    node->height = 1 + max(height(node->left), height(node->right));
-
-   
-    int balance = getBalance(node);
-
-   
-    if (balance > 1 && key < node->left->key)
-        return rightRotate(node);
-
-    
-    if (balance < -1 && key > node->right->key)
-        return leftRotate(node);
-
-
-    if (balance > 1 && key > node->left->key) {
-        node->left = leftRotate(node->left);
-        return rightRotate(node);
-    }
-
-   
-    if (balance < -1 && key < node->right->key) {
-        node->right = rightRotate(node->right);
-        return leftRotate(node);
-    }
-
-    
-    return node;
-}
-
-
-Node* deleteNode(Node* root, int key) {
-    
-    if (root == NULL)
-        return root;
-
-    
-    if (key < root->key)
-        root->left = deleteNode(root->left, key);
-
-    
-    else if (key > root->key)
-        root->right = deleteNode(root->right, key);
-
-    
-    else {
-        
-        if ((root->left == NULL) || (root->right == NULL)) {
-            Node* temp = root->left ? root->left : root->right;
-
-            
-            if (temp == NULL) {
-                temp = root;
-                root = NULL;
-            } else 
-                *root = *temp; 
-
-            free(temp);
+void enqueue(int val) {
+    if (!isFull()) {
+        if (isEmpty()) {
+            front = rear = 0;
         } else {
-            
-            Node* temp = root->right;
-            while (temp->left != NULL)
-                temp = temp->left;
+            rear = (rear + 1) % size;
+        }
+        queue[rear] = val;
+    } else {
+        printf("\nQUEUE IS FULL!\n");
+    }
+}
 
-            
-            root->key = temp->key;
+int dequeue() {
+    if (!isEmpty()) {
+        int val = queue[front];
+        if (front == rear) {
+            front = rear = -1;
+        } else {
+            front = (front + 1) % size;
+        }
+        return val;
+    } else {
+        printf("\nQUEUE IS EMPTY!\n");                              
+        return -1;
+    }
+}
 
-            root->right = deleteNode(root->right, temp->key);
+void dfs(){
+    int g[size][size]={{0,1,1,0,0,0,0},{0,0,0,0,0,0,0},{0,0,0,1,0,1,0},{1,1,0,0,0,0,1},{0,1,0,0,0,0,0},{0,0,0,0,0,0,1},{0,0,0,0,1,0,0}};
+    int visited[size]={0};
+    int j,i=0;
+   
+   printf("DFS :  ");
+    while(i>-1 && i<size)
+    {
+        if(visited[i]!=1)
+        {
+        printf("%d->",i);
+        visited[i]=1;
+        }
+        for(i,j=0;j<size;j++)
+        {
+            if(g[i][j]==1 && visited[j]!=1){
+            push(j);
+            }
+        }
+        i=pop();
+    }
+    
+}
+
+void bfs(){
+    int g[size][size] = {
+        {0, 1, 1, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 0},
+        {0, 0, 0, 1, 0, 1, 0},
+        {1, 1, 0, 0, 0, 0, 1},
+        {0, 1, 0, 0, 0, 0, 0},
+        {0, 0, 0, 0, 0, 0, 1},
+        {0, 0, 0, 0, 1, 0, 0}
+    };
+    int visited[size]={0};
+
+    int i = 0;
+    printf("BFS : ");
+    visited[i] = 1;
+    printf("%d->", i);
+
+    enqueue(i);
+
+    while (!isEmpty()) {
+        int i = dequeue();
+
+        for (int j = 0; j < size; j++) {
+            if (g[i][j] && !visited[j]) {
+                visited[j] = 1;
+                printf("%d->", j);
+                enqueue(j);
+            }
         }
     }
 
-  
-    if (root == NULL)
-        return root;
-
-    
-    root->height = 1 + max(height(root->left), height(root->right));
-
-    
-    int balance = getBalance(root);
-
-    
-    if (balance > 1 && getBalance(root->left) >= 0)
-        return rightRotate(root);
-
-
-    if (balance > 1 && getBalance(root->left) < 0) {
-        root->left = leftRotate(root->left);
-        return rightRotate(root);
-    }
-
-  
-    if (balance < -1 && getBalance(root->right) <= 0)
-        return leftRotate(root);
-
-
-    if (balance < -1 && getBalance(root->right) > 0) {
-        root->right = rightRotate(root->right);
-        return leftRotate(root);
-    }
-
-    return root;
 }
 
 
-void preOrder(Node* root) {
-    if (root != NULL) {
-        printf("%d ", root->key);
-        preOrder(root->left);
-        preOrder(root->right);
-    }
+void push(int data)
+{
+        top=top+1;
+        s[top]=data;
+   
 }
 
-
-int main() {
-    Node* root = NULL;
-    int key;
-    int n,value;
-    printf("Enter number of nodes to be inserted:");
-    scanf("%d",&n);
-    for (int i=0;i<n;i++){
-        printf("Enter data: ");
-        scanf("%d",&value);
-        root=insert(root,value);
-    }
-    
-    printf("Preorder traversal of the AVL tree after insertion: ");
-    preOrder(root);
-    printf("\n");
-
-    printf("enter key to delete: ");
-    scanf("%d",&key);
-    root = deleteNode(root,key);
-
-    printf("Preorder traversal of the AVL tree after deletion of node with key %d: ",key);
-    preOrder(root);
-    printf("\n");
-
-    return 0;
+int pop()
+{
+        int temp;
+        temp=s[top];
+        top=top-1;
+        return temp;
+   
 }
+
+int main()
+{
+    int ch,ans=1;
+    do{
+    printf("enter your choice \n1.DFS\n2.BFS\n");
+    scanf("%d",&ch);
+    switch(ch)
+    {
+        case 1:
+        dfs();
+        break;
+        
+        case 2:
+        bfs();
+        break;
+    }
+    printf("\nWant to continue ?\n1.yes \n0.no\n");
+    scanf("%d",&ans);
+    }
+    while(ans==1);
+}
+
